4d8d02e7769f5a8eebc208afd600abb03c67bbe3
Le Anh Quan <anhquanle2015@gmail.com>
2022-11-21 11:32:15 +0700

    Try

--- ConnectDB/ConnectionString.cs
+++ ConnectDB/ConnectionString.cs
@@ -9,5 +9,6 @@ namespace QuanLyNhaHang.ConnectDB
     public class ConnectionString
     {
         public static string _connectionString = "Data Source=LAPTOP-9UD23CGV;Initial Catalog=Quan_Ly_Nha_Hang;Integrated Security=True";
+    
     }
 }
